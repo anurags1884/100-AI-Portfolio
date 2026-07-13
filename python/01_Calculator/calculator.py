821eda85874d8279abc1e6c53e7ddfd51eef2622
Anurag Shinde <anurags1881@gmail.com>
2026-07-13 17:10:36 +0530

    Implement interactive calculator with all arithmetic operations

--- python/01_Calculator/calculator.py
+++ python/01_Calculator/calculator.py
@@ -1,30 +1,68 @@
 from constants import APP_NAME, VERSION, MENU
-from operations import add
+from operations import (
+    add,
+    subtract,
+    multiply,
+    divide,
+    modulus,
+    power,
+    floor_division,
+)
 from validator import get_number
+
 
 print("=" * 60)
 print(APP_NAME.center(60))
 print(f"Version {VERSION}".center(60))
 print("=" * 60)
 
-print(MENU)
 
-choice = input("Enter your choice (0-7): ")
+while True:
 
-if choice == "1":
-    print("\n------ Addition ------")
+    print(MENU)
 
-    num1 = get_number("Enter First Number : ")
-    num2 = get_number("Enter Second Number: ")
+    choice = input("Enter your choice (0-7): ")
 
-    result = add(num1, num2)
+    if choice == "0":
+        print("\nThank you for using the calculator.")
+        break
 
-    print("-" * 40)
-    print(f"Result = {result}")
-    print("-" * 40)
+    elif choice in ["1", "2", "3", "4", "5", "6", "7"]:
 
-elif choice == "0":
-    print("\nThank you for using the calculator.")
+        num1 = get_number("\nEnter First Number : ")
+        num2 = get_number("Enter Second Number: ")
 
-else:
-    print("\nFeature coming in next step...")
+        if choice == "1":
+            result = add(num1, num2)
+            operation = "+"
+
+        elif choice == "2":
+            result = subtract(num1, num2)
+            operation = "-"
+
+        elif choice == "3":
+            result = multiply(num1, num2)
+            operation = "*"
+
+        elif choice == "4":
+            result = divide(num1, num2)
+            operation = "/"
+
+        elif choice == "5":
+            result = modulus(num1, num2)
+            operation = "%"
+
+        elif choice == "6":
+            result = power(num1, num2)
+            operation = "^"
+
+        elif choice == "7":
+            result = floor_division(num1, num2)
+            operation = "//"
+
+        print("\n" + "-" * 40)
+        print(f"{num1} {operation} {num2} = {result}")
+        print("-" * 40)
+
+    else:
+        print("\n❌ Invalid Choice. Please try again.")
